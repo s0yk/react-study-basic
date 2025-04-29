2a68e4f9167a9acda8e5c6848799b4a2e2d13a4a
sykim <112912518+s0yk@users.noreply.github.com>
2025-04-29 16:31:01 +0900

    (20250429) 레이아웃 셋팅

---
+++ src/app/layout/Header.js
@@ -0,0 +1,8 @@
+'use client';
+import { HeaderWrap } from './styles';
+
+export default function Header() {
+    return (
+        <HeaderWrap>Header</HeaderWrap>
+    )
+}
---
+++ src/app/layout/styles.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const HeaderWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    padding: 24px 20px;
+    background-color:rgb(39, 35, 35);
+    color: blanchedalmond;
+    font-size: 48px;
+`
